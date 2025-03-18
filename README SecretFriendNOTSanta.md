0af2bd7f8236f4491f246777838b8804935ea87e
Checho Gonzalez <sergiogoto.works@gmail.com>
2025-03-18 18:40:13 -0500

    V1.0. IT'S USABLE AT LEAST hahaha
    Raffle works, the array of the input friends is right
    
    BONUS: Text Field cleaning is working
    
    Left to do:
    1. Invalidate and alert numbers input
    2. Invalidate and alert special characters input
    3. Re-define Roadmap of things to write and fix

---
+++ README SecretFriendNOTSanta.md
@@ -0,0 +1,25 @@
+# Secret-NOTSanta-Secret-Friend
+Base practice for the "Beginner programmer" at Alura One / Group 8
+
+15:00 GMT-8
+Just as description says, sorry, made this in a hurry the same delivery date, but I hope I can make this a bit funnier
+Base project uploaded
+
+15:32 V0.0 - 0.1
+Translated to english porque a veces uno puede ser un poquito lampara jajaja
+
+15:45
+Releases kind of organized
+
+17:30
+Created vars and consts to create the Array for friends input.
+Actions to show them visually
+
+17:52
+GOT TIRED MID EXERCISE OF DELETING TEST NAMES EACH TIME, added the fn to erase the input once you clickk add haHAHA
+
+18:14 V1.0
+The thing works, it's left to polish:
+Alert when inputting nubers
+Invalidate to input or submit/add numbers
+*Better to fix Github thingy before it's too late
--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Practice things seen at lógica de programación. Solve the new game.
+
+let friendList = [];
+let ulResult = document.getElementById("result")
+
+const inputFriend = document.getElementById("friendInput");
+const ulFriendList = document.getElementById("friendList");
+
+function addFriend() {
+    if (inputFriend.value === ""){
+        alert("Please write a name. *BRUH'")
+    }
+    friendList.push(inputFriend.value);
+    ulFriendList.innerHTML += `<li>${inputFriend.value}</li>`; 
+    cleanTxtBox();
+};
+
+
+// GOT TIRED MID EXERCISE OF DELETING TEST NAMES EACH TIME HAHA.
+//Next is the function to clean the box after adding a name.
+
+function cleanTxtBox () {
+    document.querySelector("#friendInput").value = "";
+}
+
+function raffleFriend() {
+    let randomNumber = Math.floor(Math.random()*friendList.length);
+    // log to verify number correctly generated, delimited by array length
+    //console.log(randomNumber);
+    let secretFriend = friendList[randomNumber];
+    ulResult.innerHTML = `<li>You'll spend your cash on: "${secretFriend}", lucky them.</li>`;
+    
+};
+
+console.log(friendList);
+
+//function restartRaffle() {
+    //clean box
+  //  cleanTxtBox();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    //Deshabilitar el botón de nuevo juego
+    //document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+//}
